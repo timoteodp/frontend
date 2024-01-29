d60b2c9e35af8efed8aa8df8b57f72b10b0985a3
Timóteo Dima Pereira <timoteo.pereira@mds.gov.br>
2024-01-29 20:08:49 -0300

    Noções básicas de JavaScript  Aula 02

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
-// Image switcher code
+//Código alternador de imagem
 
 let myImage = document.querySelector('img');
+
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute ('src','images/firefox2.png');
+        myImage.setAttribute ('src','images/benfica.png');
     } else {
         myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
 
-// Personalized welcome message code
+// Mensagem de Boas vindas personalizado
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
